2b39f3c83b90580d87194b9240c88ffa17574ec7
cfdxkk <b2567240058@gmail.com>
2025-03-03 06:32:21 +0000

    init

---
+++ components/background/Cube.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+	const props = defineProps({
+		width: Number,
+		height: Number,
+	})
+
+	const cubeContainerSize = computed(() => {
+		return { width: `${props.width}px`, height: `${props.height}px` }
+	})
+</script>
+
+<template>
+	<div :style="cubeContainerSize" class="cube-container">
+		
+	</div>
+</template>
+
+<style lang="css" scoped>
+	.cube-container {
+		background-color: #66ccff;
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>
+		<BackgroundCube :width="60" :height="200" />
+	</div>
+</template>
