c0dc3129486b9ad1524dde8d41a89463b248f5c3
cfdxkk <b2567240058@gmail.com>
2025-03-04 01:10:26 +0000

    move blur layer to a vue components

---
+++ components/background/Blur.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="blur"></div>
+</template>
+
+<style lang="css" scoped>
+	.blur {
+		height: 100dvh;
+		width: 100dvw;
+
+		background-color: #EEEEEE00;
+		backdrop-filter: blur(2px);
+    animation: blur_animation 8s ease-in-out 2s forwards;
+
+		position: absolute;
+		z-index: 999;
+	}
+
+	@keyframes blur_animation {
+		from {
+      background-color: #EEEEEE00;
+			backdrop-filter: blur(2px);
+		}
+		to {
+			background-color: #EEEEEECC;
+			backdrop-filter: blur(6px);
+		}
+	}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,11 @@
 
 <template>
 	<div>
-		<div class="blur"></div>
+		<BackgroundBlur />
 		<BackgroundCubeGroup />
 	</div>
 </template>
 
 <style lang="css" scoped>
-	.blur {
-		height: 100dvh;
-		width: 100dvw;
-
-		background-color: #EEEEEE00;
-		backdrop-filter: blur(2px);
-    animation: blur_animation 8s ease-in-out 2s forwards;
-
-		position: absolute;
-		z-index: 999;
-	}
-
-	@keyframes blur_animation {
-		from {
-      background-color: #EEEEEE00;
-			backdrop-filter: blur(2px);
-		}
-		to {
-			background-color: #EEEEEECC;
-			backdrop-filter: blur(6px);
-		}
-	}
+	
 </style>
