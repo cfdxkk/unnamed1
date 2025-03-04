a57828d65c98aefe5a5c130260a762a81f54d462
cfdxkk <b2567240058@gmail.com>
2025-03-04 08:29:57 +0000

    slower 3D cube rotation speed

--- components/DDDCube.vue
+++ components/DDDCube.vue
@@ -54,10 +54,10 @@
     // 动画循环
     function animate() {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      wireframe.rotation.x += 0.01;
-      wireframe.rotation.y += 0.01;
+      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.005;
+      wireframe.rotation.x += 0.005;
+      wireframe.rotation.y += 0.005;
       renderer.render(scene, camera);
     }
 
